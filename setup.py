7d7f80823d7573dd1ce278b159a82afeb8abff9c
Hud <hudwahab@gmail.com>
2020-12-18 11:15:45 -0700

    update repo name, remove onnx-tf from setuptools

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='challenge-hw01',
-    version='0.1.0',    
+    name='aidkitHW',
+    version='0.1.4',    
     description='Python package for coding challenge',
     url='https://github.com/hududed',
     author='Hud Wahab',
     author_email='hudwahab@gmail.com',
-    license='BSD 2-clause',
+    license='Apache License 2.0',
     packages=['challenge-hw01'],
     install_requires=['fastai',
                       'fastcore',
-                      'fastinferenz'
+                      'fastinferenz',
                       'timm',
                       'tensorflow==1.15',
                       'onnx', 
-                      'onnxruntime',
-                      'onnx_tf @ git+https://github.com/onnx/onnx-tensorflow.git@tf-1.x#egg=onnx_tf',
-                      'onnxconverter-common @ git+https://github.com/microsoft/onnxconverter-common@0d793b3f47b14e53a3d6dee31cdcef03b61d23ad#egg=onnxconverter-common',
+                      'onnxruntime',                
                       ],
     )
 
